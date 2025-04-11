45bfda43612f5fa2cbbca2ad972f507eaed6f4a5
Marcio Kleber Junior Lopes de Souza <marciokjls@gmail.com>
2025-04-10 22:40:52 -0300

    First commit - criação de projeto para teste do ecossistema spring

---
+++ src/test/java/com/github/marciokleber/springexperience/SpringExperienceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marciokleber.springexperience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExperienceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
