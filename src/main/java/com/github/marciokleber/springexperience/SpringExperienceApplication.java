9a3331833e8a4a9d38e5646ec7634486e693d82a
Marcio Kleber Junior Lopes de Souza <marciokjls@gmail.com>
2025-04-10 23:08:58 -0300

    Configurações iniciais do projeto.

--- src/main/java/com/github/marciokleber/springexperience/SpringExperienceApplication.java
+++ src/main/java/com/github/marciokleber/springexperience/SpringExperienceApplication.java
@@ -1,13 +1,19 @@
 package com.github.marciokleber.springexperience;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringExperienceApplication {
+public class SpringExperienceApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringExperienceApplication.class, args);
     }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
